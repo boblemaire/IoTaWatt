b9ab30e38761edc5d0967ef1ffa31d7f8863f12a
overeasy <bob@overeasy.com>
2017-01-28 14:36:13 -0500

    Update most recent changes - version to 1.3
    This version includes the trace and secure client in emonService

---
+++ IoTaWatt_1_3/Loop.ino
@@ -0,0 +1,202 @@
+void loop()
+{
+
+  // -------- This calibration is a kludge. Requires an AC cord connected
+  //          while IotaWatt in isolated running on battery and communicating
+  //          via Wifi.  The utility value is that it measures the VT phase
+  //          shift very accurately.  The standard calibration procedure in
+  //          the configuration utility allows calibrating the voltage with
+  //          just a meter or other method of reading the plug voltage.
+    
+  if(calibrationMode){
+    uint32_t calStart = millis();
+    while(calibrationMode){
+      if(calibrateVT() && calibrateVT()) calibrationMode = false ;
+      yield();
+      server.handleClient();
+      yield();
+      server.handleClient();
+      if((unsigned long)(millis() - calStart) > 20000){
+        calibrationMode = false;
+      }
+    }
+  }
+
+/******************************************************************************
+ * The main loop is very simple:
+ *  Sample a power channel.
+ *  Yield to the OS and Wifi Server.
+ *  Run next dispatchable service if there is one
+ *  Yield to the OS and Wifi Server.
+ *  Go back, Jack, and do it again.
+ ******************************************************************************/
+
+  // ------- If AC zero crossing approaching, go sample a channel.
+
+  if(millis() >= nextCrossMs){
+    GPIO.writePin(yellowLedPin,HIGH);
+    ESP.wdtFeed();
+    trace(1);
+    samplePower(nextChannel);
+    trace(2);
+    GPIO.writePin(yellowLedPin,LOW); 
+    nextCrossMs = lastCrossMs + 490 / int(frequency);
+    nextChannel = ++nextChannel % channels;
+  }
+
+  yield();
+  ESP.wdtFeed();
+  trace(3);
+  server.handleClient();
+  trace(4);
+  yield();
+
+// ---------- If the head of the service queue is dispatchable
+//            invoke it's service function.
+
+  if(serviceQueue != NULL && NTPtime() >= serviceQueue->callTime){
+    serviceBlock* thisBlock = serviceQueue;
+    serviceQueue = thisBlock->next;
+    ESP.wdtFeed();
+    trace(5);
+    thisBlock->callTime = thisBlock->service(thisBlock);
+    trace(6);
+    AddService(thisBlock); 
+  }
+     
+  yield();
+  ESP.wdtFeed();
+  trace(7);
+  server.handleClient();
+  trace(8);
+  yield();
+}
+
+/*********************************************************************************************************
+ * Scheduler/Dispatcher support functions.
+ * 
+ * The main loop steps through and samples the channels at the millisecond level.  It invokes samplePower()
+ * which samples one or more waves and updates the corresponding data buckets.  After each sample, there 
+ * are a few milliseconds before the next AC zero crossing, so we try to do everything else during that
+ * downtime.
+ * 
+ * To accomplish that, other activities are organized as SERVICEs that are scheduled and are dispatched in 
+ * Loop during the half-cycle downtime. The ESP8266 is already running a lower level operating system that 
+ * is task oriented and dispatches this program along with other tasks - most notably the WiFi stack. Ticker
+ * taps into that and provides one time or periodic interrupts that could be used to run services, but we
+ * run the channel sampling with interrupts disabled, and we really don't need sub-second scheduling for 
+ * our services anyway, so Ticker is not used.
+ * 
+ * This mechanism schedules at a resolution of one second, and dispatches during the optimal time period
+ * between AC cycles.  To avoid context and synchonization issues, each service is coded as a state-machine.
+ * They must be well behaved and should try to run for less than a few milliseconds. Although that isn't
+ * always possible.
+ * 
+ * Services return the UNIXtime of the next requested dispatch.  If the requested time is in the past 
+ * (includes 0), the service is requeued at the current time.  
+ * 
+ * The schedule itself is kept as an ordered list of control blocks in ascending order of time + priority.
+ * Loop simply invokes the service currently at the beginning of the list.
+ * 
+ * The WiFi server is not one of these services.  It is invoked each time through the loop because it
+ * polls for activity.
+ ********************************************************************************************************/
+
+void NewService(uint32_t (*serviceFunction)(struct serviceBlock*)){
+    serviceBlock* newBlock = new serviceBlock;
+    newBlock->service = serviceFunction;
+    AddService (newBlock);
+  }
+
+void AddService(struct serviceBlock* newBlock){
+  if(newBlock->callTime < NTPtime()) newBlock->callTime = NTPtime();
+  if(serviceQueue == NULL ||
+    (newBlock->callTime < serviceQueue->callTime) ||
+    (newBlock->callTime == serviceQueue->callTime && newBlock->priority < serviceQueue->priority)){
+    newBlock->next = serviceQueue;
+    serviceQueue = newBlock;
+  }
+  else {
+    serviceBlock* link = serviceQueue;
+    while(link->next != NULL){
+      if((newBlock->callTime < link->next->callTime) ||
+        (newBlock->callTime == link->next->callTime && newBlock->priority < link->next->priority)){
+        break;
+      }        
+      link = link->next;
+    } 
+    newBlock->next = link->next;
+    link->next = newBlock;
+  }
+}
+
+/******************************************************************************************************** 
+ * All of the other SERVICEs that harvest values from the main "buckets" do so for their own selfish 
+ * purposes, and have no global scope to share with others. This simple service maintains periodic
+ * values for each of the buckets in a global set of buckets called statBuckets.  It also is where 
+ * status statistics like sample rates are maintained.  It runs at relatively low frequency (10 sec)
+ * but is reved up while there is an active status session querrying the server.
+ *******************************************************************************************************/
+
+uint32_t statService(struct serviceBlock* _serviceBlock) {
+  static uint32_t timeThen = millis() - 1;            // Don't want divide by zero on first call
+  uint32_t timeNow = millis();
+
+  if(float(cycleSamples) > 0){
+    cycleSampleRate = cycleSampleRate * .75 + .25 * float(cycleSamples * 1000) / float((uint32_t)(timeNow - timeThen));
+  } else {
+    cycleSampleRate = cycleSampleRate * .75;
+  }
+  
+  double elapsedHrs = double((uint32_t)(timeNow - timeThen)) / MS_PER_HOUR;
+  for(int i=0; i<channels; i++){
+    ageBucket(&buckets[i],timeNow);
+    statBuckets[i].value1 = (buckets[i].accum1 - statBuckets[i].accum1) / elapsedHrs;
+    statBuckets[i].value2 = (buckets[i].accum2 - statBuckets[i].accum2) / elapsedHrs;
+    statBuckets[i].value3 = (buckets[i].accum3 - statBuckets[i].accum3) / elapsedHrs;
+    statBuckets[i].accum1 = buckets[i].accum1;
+    statBuckets[i].accum2 = buckets[i].accum2;
+    statBuckets[i].accum3 = buckets[i].accum3;
+  }
+    
+  timeThen = timeNow;
+  cycleSamples = 0;
+  
+  if(statServiceInterval < 10)statServiceInterval++;
+  return ((uint32_t)NTPtime() + statServiceInterval);
+}
+
+void trace(uint32_t id){
+  static uint16_t traceSeq = 0;
+  id |= traceSeq++ << 16;
+  WRITE_PERI_REG(RTC_USER_MEM + 96 + (traceSeq & 0x1F), id);
+}
+
+void printTrace(void){
+  uint16_t _counter = READ_PERI_REG(RTC_USER_MEM + 96) >> 16;
+  Serial.println(formatHex(_counter,4));
+  int i=1;
+  while(((uint16_t)++_counter) == (READ_PERI_REG(RTC_USER_MEM + 96 + (i%32)) >> 16)) i++;
+  Serial.println(i);
+  for(int j=0; j<32; j++){
+    Serial.print(formatHex(READ_PERI_REG(RTC_USER_MEM + 96 + ((j+i)%32)), 4));
+    Serial.println();
+  }
+}
+
+void logTrace(void){
+  uint16_t _counter = READ_PERI_REG(RTC_USER_MEM + 96) >> 16;
+  int i = 1;
+  while(((uint16_t)++_counter) == (READ_PERI_REG(RTC_USER_MEM + 96 + (i%32)) >> 16)) i++;
+  String line = "Trace: ";
+  for(int j=0; j<32; j++){
+    uint32_t _entry = READ_PERI_REG(RTC_USER_MEM + 96 + ((j+i)%32)) & 0xFFFF;
+    line += String(_entry) + ",";
+  }
+  line.remove(line.length()-1);  
+  msgLog(line);
+}
+
+
+
+
---
+++ IoTaWatt_1_3/eMonService.ino
@@ -0,0 +1,265 @@
+  /*******************************************************************************************************
+ * eMonService - This SERVICE posts entries from the IotaLog to eMonCMS.  Details of the eMonCMS
+ * account are provided in the configuration file at startup and this SERVICE is scheduled.  It runs
+ * more or less independent of everything else, just reading the log records as they become available
+ * and sending the data out.
+ * The advantage of doing it this way is that there is really no eMonCMS specific code anywhere else
+ * except a speciific section in getConfig.  Other web data logging services could be handled
+ * the same way.
+ * It's possible that multiple web services could be updated independently, each having their own 
+ * SERVER.  The only issue right now would be the WiFi resource.  A future move to the 
+ * asynchWifiClient would solve that.
+ ******************************************************************************************************/
+uint32_t eMonService(struct serviceBlock* _serviceBlock){
+  enum   states {initialize, post, resend};
+  static states state = initialize;
+  static IotaLogRecord* logRecord = new IotaLogRecord;
+  static double accum1Then [channels];
+  static double accum2Then [channels];
+  static uint32_t UnixLastPost = UnixTime();
+  static uint32_t UnixNextPost = UnixTime();
+  static double _logHours;
+  static String req = "";  
+  static uint32_t postTime = millis();
+
+  trace(10);    
+  switch(state){
+
+    case initialize: {
+
+          // We post the log to EmonCMS,
+          // so wait until the log service is up and running.
+      
+      if(!iotaLog.isOpen()){
+        return ((uint32_t) NTPtime() + 5);
+      }
+      msgLog("EmonCMS service started.");
+
+          // Get the last record in the log.
+          // Posting will begin with the next log entry after this one,
+          // so in the future if there is a way to remember the last
+          // that was posted (RTC memory or maybe use the SPIFFS) then 
+          // that record should be loaded here. Benchmarks indicate 
+          // can upload about three per second, so maybe put some 
+          // limits on the lookback.
+          
+      logRecord->UNIXtime = iotaLog.lastKey();
+      iotaLog.readKey(logRecord);
+
+          // Save the value*hrs to date, and logHours to date
+      
+      for(int i=0; i<channels; i++){ 
+        accum1Then[i] = logRecord->channel[i].accum1;
+        accum2Then[i] = logRecord->channel[i].accum2;
+      }
+      _logHours = logRecord->logHours;
+
+          // Assume that record was posted (not important).
+          // Plan to start posting one interval later
+      
+      UnixLastPost = logRecord->UNIXtime;
+      UnixNextPost = UnixLastPost + eMonCMSInterval - (UnixLastPost % eMonCMSInterval);
+
+          // Advance state.
+          // Set task priority low so that datalog will run before this.
+      
+      state = post;
+      _serviceBlock->priority = priorityLow;
+      return ((uint32_t) UnixNextPost + SEVENTY_YEAR_SECONDS);
+    }
+
+        
+    case post: {
+
+          // If we are current,
+          // Anticipate next posting at next regular interval and break to reschedule.
+ 
+      if(iotaLog.lastKey() < UnixNextPost){
+        
+        UnixNextPost = UnixTime() + eMonCMSInterval - (UnixTime() % eMonCMSInterval);
+        return ((uint32_t)UnixNextPost + SEVENTY_YEAR_SECONDS);
+      } 
+      
+          // Not current.  Read sequentially to get the entry >= scheduled post time
+      
+      while(logRecord->UNIXtime < UnixNextPost){
+        if(logRecord->UNIXtime >= iotaLog.lastKey()){
+          msgLog("runaway seq read.", logRecord->UNIXtime);
+          ESP.reset();
+        }
+        iotaLog.readNext(logRecord);
+      }
+
+          // Adjust the posting time to match the log entry time.
+      
+      UnixNextPost = logRecord->UNIXtime - logRecord->UNIXtime % eMonCMSInterval;
+
+          // Build the request string.
+          // values for each channel are (delta value hrs)/(delta log hours) = period value.
+          // Update the previous (Then) buckets to the most recent values.
+     // req = "GET " + eMonURL.c_str() + " HTTP/1.1\r\n" +
+
+      req = "/input/post.json?time=" + String(UnixNextPost) + "&node=" + String(node) + "&csv=";    
+      int commas = 0;
+      double value1;
+      double elapsedHours = logRecord->logHours - _logHours;
+      _logHours = logRecord->logHours;
+      for (int i = 0; i < channels; i++) {
+        value1 = (logRecord->channel[i].accum1 - accum1Then[i]) / elapsedHours;
+        accum1Then[i] = logRecord->channel[i].accum1;
+        accum2Then[i] = logRecord->channel[i].accum2;
+        if(channelType[i] != channelTypeUndefined) {
+          while(commas > 0) {
+            req += ",";
+            commas--;
+          }
+          if(channelType[i] == channelTypeVoltage) req += String(value1,1);
+          else if(channelType[i] == channelTypePower) req += String(long(value1+0.5));
+          else req += String(long(value1+0.5));
+        }
+        commas++;
+      }
+      req += "&apikey=" + apiKey;
+
+          // Send the post       
+      
+      uint32_t sendTime = millis();
+      if(!eMonSend(req)){
+        state = resend;
+        return ((uint32_t)NTPtime() + 1);
+      }
+      Serial.print(formatHMS(NTPtime() + (localTimeDiff * 3600)));
+      Serial.print(" ");
+      Serial.print(millis()-sendTime);
+      Serial.print(" ");
+      Serial.println(req);     
+      UnixLastPost = UnixNextPost;
+      UnixNextPost +=  eMonCMSInterval - (UnixNextPost % eMonCMSInterval);
+      state = post;
+      
+      return ((uint32_t)NTPtime() + 1);
+    }
+
+
+    case resend: {
+      msgLog("Resending eMonCMS data.");
+      if(!eMonSend(req)){
+        return ((uint32_t)NTPtime() + 5);
+      }
+      else {
+        
+        UnixLastPost = UnixNextPost;
+        UnixNextPost +=  eMonCMSInterval - (UnixNextPost % eMonCMSInterval);
+        state = post;
+        return ((uint32_t)NTPtime() + 1);
+      }
+      break;
+    }
+  }
+  return 0;
+}
+
+/************************************************************************************************
+ *  eMonSend - send data to the eMonCMS server. 
+ *  if secure transmission is configured, pas sthe request to a 
+ *  similar WiFiClientSecure function.
+ *  Secure takes about twice as long and can block sampling for more than a second.
+ ***********************************************************************************************/
+boolean eMonSend(String req){
+  
+  if(eMonSecure) return eMonSendSecure(req);
+  trace(11);
+  
+  uint32_t startTime = millis();
+  if(!WifiClient.connect(eMonURL.c_str(), 80)) {
+        msgLog("failed to connect to:", eMonURL);
+        WifiClient.stop();
+        return false;
+  } 
+  yield();  
+  WifiClient.println(String("GET ") + req);
+  uint32_t _time = millis();
+  while(WifiClient.available() < 2){
+    yield();
+    if((uint32_t)millis()-_time >= 200){
+      msgLog("eMonCMS timeout.");
+      WifiClient.stop();
+      return false;
+    }
+  }
+  yield();
+  String reply = "";
+  int maxlen = 40;
+  while(WifiClient.available()){
+    reply += (char)WifiClient.read();
+    if(!maxlen--){
+      break;
+    }
+  }
+  if(reply.substring(0,2) != "ok"){
+    msgLog("eMonCMS reply: ", reply);
+    WifiClient.stop();
+    return false;
+  }
+  WifiClient.stop();
+//  Serial.print("Open Send ms: ");
+//  Serial.println(millis()-startTime);
+  return true;
+}
+
+boolean eMonSendSecure(String req){
+  trace(12);
+  ESP.wdtFeed();
+
+      // Should always be disconnected, but test can't hurt.
+    
+  uint32_t startTime = millis();
+  if(!WifiClientSecure.connected()){
+    if(!WifiClientSecure.connect(eMonURL.c_str(), HttpsPort)) {
+          msgLog("failed to connect to:",  eMonURL);
+          WifiClientSecure.stop();
+          return false;
+    }
+    if(!WifiClientSecure.verify(eMonSHA1,  eMonURL.c_str())){
+      msgLog("eMonCMS could not validate certificate.");
+      WifiClientSecure.stop();
+      return false;
+    }
+  }
+  yield();
+  
+      // Send the packet
+   
+  WifiClientSecure.print(String("GET ") + req + " HTTP/1.1\r\n" +
+               "Host: " + eMonURL + "\r\n" +
+               "User-Agent: IotaWatt\r\n" +
+               "Connection: close\r\n\r\n"); 
+ 
+      // Read through response header until blank line (\r\n)
+
+  yield();    
+  while (WifiClientSecure.connected()) {
+    String line = WifiClientSecure.readStringUntil('\n');
+    if (line == "\r") {
+      break;
+    }
+  }
+
+  yield(); 
+  String line;
+  while(WifiClientSecure.available()){
+    line += char(WifiClientSecure.read());
+  }
+  if (!line.startsWith("ok")) {
+    msgLog ("eMonCMS reply: ", line);
+    WifiClientSecure.stop();
+    return false;
+  }              
+  
+//  Serial.print("Secure Send ms: ");
+//  Serial.println(millis()-startTime);
+  return true;
+}
+
+
+
