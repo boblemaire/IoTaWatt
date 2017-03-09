4f5e8c3de56444b4776e99c5335c9faa8fd42415
overeasy <bob@overeasy.com>
2017-03-08 21:07:26 -0500

    Recognize shortcut /edit and /graph in web server
    Append .htm to /edit and /graph uri

--- IoTaWatt_2_0/Changelog.ino
+++ IoTaWatt_2_0/Changelog.ino
@@ -1,9 +1,9 @@
 /*********************************** Change log ****************************************************
  *  
- *   3/5/17 Cleaned up and added more documentation to Sample Power.  Also streamlined
- *          it a little more and fixed a few loose ends.
+ *   03/05/17 2.00.01 Cleaned up and added more documentation to Sample Power.  Also streamlined
+ *                    it a little more and fixed a few loose ends.
  *  
- *  
+ *   03/08/17 2.00.02 Recognize /edit and /graph uri in server
  *  
  *  
  *  
