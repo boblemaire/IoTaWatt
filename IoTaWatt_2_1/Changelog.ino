710b2ca648dd5c6d6c5c9b2b756b7c6b700eb750
Bob Lemaire <bob@overeasy.com>
2017-07-09 12:48:39 -0400

    Bump Version
    Bump to vesion 2.01.05

--- IoTaWatt_2_1/Changelog.ino
+++ IoTaWatt_2_1/Changelog.ino
@@ -18,5 +18,6 @@
  *   05/06/17 2.01.02 Rework samplePower for better phase correction
  *   05/28/17 2.01.03 Add automatic update and rework WiFiManager use
  *   06/04/17 2.01.04 Miscelaneous cleanup
+ *   06/26/17 2.01.05 Ongoing development
  *   
  *****************************************************************************************************/
