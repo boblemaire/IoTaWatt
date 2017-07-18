edaefd0aee185d05389e27d935603d924effee7b
Bob Lemaire <bob@overeasy.com>
2017-07-17 22:07:24 -0400

    Sample power tune-up
    Fix problems detecting no VT connected.
    Narrow range for offsets to .5%

--- Firmware/IotaWatt/Changelog.ino
+++ Firmware/IotaWatt/Changelog.ino
@@ -22,5 +22,6 @@
  *   07/09/17 2.02.00 Version 4 hardware support
  *   07/12/17 2.02.01 Fix sample power, enhance graph
  *   07/15/17 2.02.02 Enhance status display
+ *   07/17/17 2.02.03 Fix problems with sample power & multiple V channels
  *   
  *****************************************************************************************************/
