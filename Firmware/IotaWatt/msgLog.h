7a86741f7fc303fb862683c36dc9758d5e2fcb84
Bob Lemaire <bob@overeasy.com>
2018-02-14 10:42:51 -0500

    Fix unsigned parameter in msgLog
    Second parameter overload was unsigned.  Make signed.

--- Firmware/IotaWatt/msgLog.h
+++ Firmware/IotaWatt/msgLog.h
@@ -3,7 +3,7 @@
 
 void      msgLog(String);
 void      msgLog(const char*, String);
-void      msgLog(const char*, uint32_t);
+void      msgLog(const char*, int32_t);
 void      msgLog(const char*);
 void      msgLog(const char*, const char*);
 void      msgLog(const char*, const char*, const char*);
