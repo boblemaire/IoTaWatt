66844127ef0ad8e7b8bba41e6c8fa452dd0e6bc6
Bob Lemaire <bob@overeasy.com>
2018-07-19 15:26:05 -0400

    Make getall more efficient with String and chunks
    Also, move literals to progmem

--- Firmware/IotaWatt/graphSave.cpp
+++ Firmware/IotaWatt/graphSave.cpp
@@ -25,52 +25,64 @@ void handleGraphCreate(){
   SD.remove(filePath);
   graphFile = SD.open(filePath, FILE_WRITE);
   if( ! graphFile){
-    returnFail("Couldn't create file.");
+    server.send(500, txtPlain_P, F("Couldn't create file."));
     return;
-  }
+  } 
   graphFile.write(server.arg("data").c_str());
   graphFile.close();
-  String response = "{\"success\":true,\"message\":\"graph saved id:" + fileName + "\"}";
-  server.send(200, "text/json", response);  
+  String response(F("{\"success\":true,\"message\":\"graph saved id:"));
+  response += fileName + "\"}";
+  server.send(200, appJson_P, response);  
   return;
 }
 
 void handleGraphDelete(){
   String filePath = graphDir;
   String fileName = server.arg("id");
-  filePath += "/" + fileName + ".txt";
+  filePath += "/" + fileName;
+  if(!fileName.endsWith(".txt")){
+    fileName += ".txt";
+  }
   SD.remove(filePath);
-  server.send(200, "text/json", "{\"success\":true,\"message\":\"deleted\"}");
+  server.send(200, txtJson_P, F("{\"success\":true,\"message\":\"deleted\"}"));
 }
 
 void handleGraphGetall(){
   File directory = SD.open(graphDir);
-  if(! (directory or directory.isDirectory())){
-    server.send(200, "application/json", String("[]"));
+  File graphFile;
+  if( !(directory) || !(directory.isDirectory())){
+    server.send(200, appJson_P, "[]");
+    return;
+  }
+  if( !(graphFile = directory.openNextFile())){
+    directory.close();
+    server.send(200, appJson_P, "[]");
     return;
   }
   server.setContentLength(CONTENT_LENGTH_UNKNOWN);
-  server.send(200,"application/json","");
-  server.sendContent("[");
-  bool first = true;
-  File graphFile;
-  uint8_t* bufr;
-  while(graphFile = directory.openNextFile()){
-    if( ! first){
-      server.sendContent(",");
-    }
-    first = false;
+  server.send(200, appJson_P, "");
+  String response = "[";
+  while(graphFile){
     uint32_t fileSize = graphFile.size();
-    bufr = new uint8_t[fileSize+17];
+    char* bufr = new char[fileSize];
     graphFile.read(bufr, fileSize);
-    strcpy((char*)bufr+fileSize-1,",\"id\":\"filename\"}");
-    memcpy((char*)bufr+fileSize+6, graphFile.name(),8);
-    server.sendContent(String((char*)bufr));
-    graphFile.close();
+    bufr[fileSize-1] = 0;
+    response += bufr;
     delete[] bufr;
+    graphFile.close();
+    response += ",\"id\":\"";
+    response += graphFile.name();
+    response += "\"},";
+    graphFile.close();
+    graphFile = directory.openNextFile();
+    if( ! graphFile){
+      response[response.length()-1] = ']';
+    }
+    server.sendContent(response);
+    response.remove(0);
   }
-  server.sendContent("]");
-  server.sendContent("");
+  server.sendContent(response);
+  server.client().stop();
   directory.close();
   return;
 }
