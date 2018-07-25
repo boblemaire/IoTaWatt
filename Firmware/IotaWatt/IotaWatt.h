513056cd70240e51d7d8f91ec501945b816e4903
Bob Lemaire <bob@overeasy.com>
2018-07-25 16:47:24 -0400

    Version 02_03_10
    Use staging core
    Leaner auth session policy
    Run getfeeddata in handler

--- Firmware/IotaWatt/IotaWatt.h
+++ Firmware/IotaWatt/IotaWatt.h
@@ -19,7 +19,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.  
    
 ***********************************************************************************/
-#define IOTAWATT_VERSION "02_03_09"
+#define IOTAWATT_VERSION "02_03_10"
 
 #define PRINT(txt,val) Serial.print(txt); Serial.print(val);      // Quick debug aids
 #define PRINTL(txt,val) Serial.print(txt); Serial.println(val);
