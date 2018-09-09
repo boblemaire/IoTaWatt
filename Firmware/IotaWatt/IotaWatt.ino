a5bde83ae2067d288ec205b0a27caf2869cd2d11
Bob Lemaire <bob@overeasy.com>
2018-09-08 20:42:26 -0400

    Version 02_03_14

--- Firmware/IotaWatt/IotaWatt.ino
+++ Firmware/IotaWatt/IotaWatt.ino
@@ -80,7 +80,8 @@
  *   07/25/18 02_03_10 run getFeedData in handler, auth session changes, use staging core 
  *   07/31/18 02_03_11 Minor changes for general release 
  *   08/08/18 02_03_12 Finally fix memory leak?
- *   08/11/18 02_03_13 Back to 2.4.1 core, overhaul timeservice, fix zero voltage, add RSSI for WiFi                                 
+ *   08/11/18 02_03_13 Back to 2.4.1 core, overhaul timeservice, fix zero voltage, add RSSI for WiFi 
+ *   09/08/18 02_03_14 Add reverse and double to inputs, modify sampleCycle                                 
  * 
  *****************************************************************************************************/
 
