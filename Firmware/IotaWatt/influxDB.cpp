685d0832a134d5b8bae983cddf0719cc6f436caf
overeasy <boblemaire@users.noreply.github.com>
2020-02-09 15:39:30 -0500

    Update influxDB.cpp

--- Firmware/IotaWatt/influxDB.cpp
+++ Firmware/IotaWatt/influxDB.cpp
@@ -532,7 +532,7 @@ bool influxConfig(const char* configObj){
   
   influxURL->parse(config.get<char*>("url"));
   influxURL->query(nullptr);
-  if(influxURL->port() <= 0){
+  if( ! influxURL->port()){
     influxURL->port(":8086");
   }
   trace(T_influxConfig,4);
